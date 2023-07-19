f05e01bfad634ac2f0eaa50d593d5ed05b2bf41f
Kevin Le <Kevin.Le@fcma.com>
2023-07-19 13:44:26 -0400

    implement fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
-# add your code here
+# Week 2: Bonus (Python_1)
 
+def fizz_buzz(num):
+    result = ""
+    if num % 3 == 0:
+        result += "Fizz"
+    if num % 5 == 0:
+        result += "Buzz"
+    if len(result) == 0:
+        result = str(num)
+    return result
+
+finish = [print(fizz_buzz(num)) for num in range(1, 101)]
